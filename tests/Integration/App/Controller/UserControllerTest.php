a612620f6318d5e4f76da25985ec6bed221f41f1
Ben Cross <bencross86@gmail.com>
2019-10-23 19:36:16 +0100

    Added usercontroller test for list endpoint

--- tests/Integration/App/Controller/UserControllerTest.php
+++ tests/Integration/App/Controller/UserControllerTest.php
@@ -27,10 +27,30 @@ class UserControllerTest extends IntegrationTest
         $this->loadData($users);
 
         $client = $this->createClient();
-
         $client->request('GET', '/users');
 
-        d($client->getResponse());
-        die;
+        $response = $client->getResponse();
+        $body = json_decode($response->getContent(), true);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertArrayHasKey('data', $body);
+        $this->assertArrayHasKey('meta', $body);
+
+        $this->assertIsArray($body['data']);
+        $this->assertCount(10, $body['data']);
+
+        foreach ($body['data'] as $user) {
+            $this->assertArrayHasKey('id', $user);
+            $this->assertArrayHasKey('username', $user);
+            $this->assertArrayHasKey('email', $user);
+            $this->assertArrayHasKey('createdAt', $user);
+            $this->assertArrayHasKey('updatedAt', $user);
+        }
+
+        $this->assertArrayHasKey('uri', $body['meta']);
+        $this->assertEquals($client->getRequest()->getRequestUri(), $body['meta']['uri']);
+
+        $this->assertArrayHasKey('resultCount', $body['meta']);
+        $this->assertEquals(10, $body['meta']['resultCount']);
     }
 }
